131f848308e83eb694756b0a7289710e98a9f608
Marcos <vimarcos188@gmail.com>
2024-07-09 16:25:52 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJETO_XUNIT
+{
+    internal static class Calculadora
+    {
+        public static double Somar(double x, double y) 
+        {
+            return x + y; 
+        }
+        
+        public static double Subtrair(double x, double y)
+        {
+            return x - y;
+        }
+        
+        public static double Multiplicar(double x, double y)
+        {
+            return x * y;
+        }
+        
+        public static double Dividir(double x, double y)
+        {
+            return x / y;
+        }
+
+
+
+
+    }
+
+
+}
---
+++ TesteCalculadora.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace PROJETO_XUNIT
+{
+    public class TesteCalculadora
+    {
+        [Fact]
+        public void TestarSomar()
+        {
+            //Given
+
+
+            //When
+            double resultado = Calculadora.Somar(57, 22);
+
+            //Then
+            Assert.Equal(79, resultado);
+        }
+
+        [Fact]
+        public void TestarSubtracao()
+        {
+            //Given
+
+
+            //When
+            double resultado = Calculadora.Subtrair(57, 22);
+
+            //Then
+            Assert.Equal(35, resultado);
+        }
+
+        [Fact]
+        public void TestarDividir()
+        {
+            //Given
+
+
+            //When
+            double resultado = Calculadora.Dividir(25, 5);
+
+            //Then
+            Assert.Equal(5, resultado);
+        }
+
+        [Fact]
+        public void TestarMultiplicacao()
+        {
+            //Given
+
+
+            //When
+            double resultado = Calculadora.Multiplicar(4, 2);
+
+            //Then
+            Assert.Equal(8, resultado);
+        }
+
+        [Fact]
+        public void TestarInfito()
+        {
+            //Given
+
+
+            //When
+            double resultado = Calculadora.Dividir(2, 0);
+
+            //Then
+            Assert.Equal(double.PositiveInfinity, resultado);
+        }
+    }
+}
